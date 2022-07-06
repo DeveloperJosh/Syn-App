1889957a5d23b796371bd06199bac41fefe891d1
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-07-06 15:02:10 -0400

    Doing some testing on the app before i really start building an app with this

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import '../dist/output.css';
-import { createRoot } from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 
-//// take data file from input and parse it into an array of objects
-function newpage() {
-  //// add now loading spinner then load new page
+function StartPage() {
   const element = document.getElementById('root');
-  const root = createRoot(element!);
-  root.render(<>
-    <div className="container mx-auto px-4 py-52">
-      <div className="flex flex-col items-center">
-        <h1 className="text-white text-3xl font-bold">Not Done Yet</h1>
-        <br />
-        <button onClick={render} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Go Back
-        </button>
-        </div>
-      </div>
-    </>
-  );
+  const root = ReactDOMClient.createRoot(element!);
+  root.render(
+  <div className="container mx-auto px-4 py-52">
+  <div className="flex flex-col items-center">
+    <h1 className="text-white text-3xl font-bold">SynDB</h1>
+    <p className="text-white text-xl">
+      A simple, yet powerful, database system.
+    </p>
+    <br />
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={render}>
+      Home2
+    </button>
+  </div>
+</div>
+);
 }
 
 function render() {
   /// use createRoot
   const element = document.getElementById('root');
-  const root = createRoot(element!);
+  const root = ReactDOMClient.createRoot(element!);
   root.render(
       <div className="container mx-auto px-4 py-52">
         <div className="flex flex-col items-center">
@@ -34,8 +34,8 @@ function render() {
             A simple, yet powerful, database system.
           </p>
           <br />
-          <button onClick={newpage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Get Started
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={StartPage}>
+            Home1
           </button>
         </div>
       </div>
@@ -43,4 +43,3 @@ function render() {
 }
 
 render();
-
--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const createWindow = (): void => {
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-  mainWindow.menuBarVisible = false;
+  mainWindow.menuBarVisible = true;
 };
 
 // This method will be called when Electron has finished
